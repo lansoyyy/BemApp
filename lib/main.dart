89866ad08f8ebe710db81f01347ff59caaa2ea69
Lans12345 <olanalans12345@gmail.com>
2025-12-18 06:52:58 +0800

    updated app name

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Light Signal App',
+      title: 'Bem App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
